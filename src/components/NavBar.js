24337ff1dd15d1a128d665a99930649fd7fa3f57
Dan Possehl <danielbpossehl@gmail.com>
2021-11-15 10:22:29 -0700

    added navbar and routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import "../styles/nav_bar-styles.css";
+
 function NavBar() {
-  return <div></div>;
+  return (
+    <div class="nav_bar">
+      <div clasname="icon_container menu_switch"></div>
+
+      <div className="nav_container">
+        <div className="icon_container logo"></div>
+        <ul className="menu_items">
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
